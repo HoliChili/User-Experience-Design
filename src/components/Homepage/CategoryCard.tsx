502df034f5e22a0762d37c2b6b9fae623f3807dd
HoliChili <157495461+HoliChili@users.noreply.github.com>
2024-09-04 21:38:19 +0800

    Update CategoryCard.tsx

--- src/components/Homepage/CategoryCard.tsx
+++ src/components/Homepage/CategoryCard.tsx
@@ -1,33 +1,38 @@
 import React from "react";
 import styles from "./Homepage.module.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 interface CategoryCardProps {
   title: string;
   imageSrc: string;
   backgroundColor: string;
   width?: string;
+  link: string;
 }
 
 const CategoryCard: React.FC<CategoryCardProps> = ({
   title,
   imageSrc,
   backgroundColor,
-  width}) => {
+  width,
+  link,}) => {
   return (
-    <div
-      className={`${styles.categoryCard} ${
-        styles[`categoryCard--${title.toLowerCase()}`]
-      }`}
-      style={{ backgroundColor, width }}
-    >
-      <img
-        loading="lazy"
-        src={imageSrc}
-        alt={`${title} category`}
-        className={styles.categoryImage}
-      />
-      <div className={styles.categoryTitle}>{title}</div>
-    </div>
+    <Link to={`/${link}`} style={{ color: 'inherit' ,textDecoration: 'none'}}>
+      <div
+        className={`${styles.categoryCard} ${
+          styles[`categoryCard--${title.toLowerCase()}`]
+        }`}
+        style={{ backgroundColor, width }}
+      >
+        <img
+          loading="lazy"
+          src={imageSrc}
+          alt={`${title} category`}
+          className={styles.categoryImage}
+        />
+        <div className={styles.categoryTitle}>{title}</div>
+      </div>
+    </Link>
   );
 };
 
